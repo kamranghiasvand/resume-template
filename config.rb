493575f1200dccf8423ec1470ad74574c1b7c6e8
kamran ghiasvand <kamran.ghiasvand@gmail.com>
2020-08-09 19:53:30 +0430

    little changes

--- config.rb
+++ config.rb
@@ -2,4 +2,4 @@ require "susy"
 css_dir = 'dist/css'
 sass_dir = 'src/sass'
 javascript_dir = 'dist/js'
-output_style = :compressed
+output_style = :expanded
---
+++ src/data1.js
@@ -0,0 +1,92 @@
+data = {
+  photo_path: "./pic/photo.jpg",
+  person_info: {
+    name: "Kamran Ghiasvand",
+    tel: {
+      showing_text: "+98-933-393-8680",
+      value: "+98-933-393-8680",
+    },
+    email: {
+      showing_text: "Kamran.Ghiasvand@gmail.com",
+      value: "Kamran.Ghiasvand@gmail.com",
+    },
+    linkedin: {
+      showing_text: "kamran-ghiasvand",
+      value: "https://www.linkedin.com/in/kamran-ghiasvand/",
+    },
+    skype: {
+      showing_text: "kamran.ghiasvand",
+      value: "skype:kamran.ghiasvand?chat",
+    },
+  },
+  education: [
+    {
+      title: "MASTER OF INFORMATION SECURITY",
+      body: "Malek Ashtar University of Technology, Iran | 2016",
+    },
+    {
+      title: "BACHELOR OF INFORMATION TECHNOLOGY",
+      body: "Hamedan University of Technology, Hamedan, Iran | 2012",
+    },
+    {
+      title: "DIPLOMA OF MATHEMATICS AND PHYSICS",
+      body: " Shahid Beheshti School, Borojerd, Iran | 2010",
+    },
+  ],
+  langs: [
+    {
+      name: "PERSIAN",
+      expertise: 100,
+    },
+    {
+      name: "ENGLISH",
+      expertise: 80,
+    },
+    {
+      name: "GERMAN",
+      expertise: 40,
+    },
+  ],
+  summary:
+    "Accomplished Software developer eager to leverage 6+ years of success in Information Technology for large-scale companies. Fluent in JAVA technology stacks with particular skills in large scale application using Spring technologies. Experienced in full life-cycle development and implementation of large scale, real-time applications and event-driven enterprise application architectures. Develops efficient enterprise level software using modern technologies such as Kafka, MongoDB and REDIS. Defines technical requirements for the implementation of a telecommunication software. Determines project objectives, requirements and scopes. Excels with backend development and REST APIs. Designed and developed over 20 advanced applications from use cases and functional requirement.",
+  work_experience: [{}],
+};
+
+d = {
+  person: {
+    firstName: "Kamran",
+    lastName: "Ghiasvand",
+    birthDay: "2012-04-23",
+    email: "kamran.ghiasvand@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/kamran-ghiasvand/",
+    skype: "skype:kamran.ghiasvand?chat",
+    photo: "./pic/photo.jpg",
+    photo_tb: "./pic/photo_tb.jpg",
+  },
+  education: [
+    {
+      title: "Information Security",
+      grade: "Master",
+      school: "Malek-e-Ashtar University",
+      country: "Iran",
+      startYear: "2013",
+      endYear: "2016",
+      Dissertation:
+        "Proposing a metric for measure software system security against SQL injection attacks",
+      Supervisor: "Dr. Ali Hadavi",
+    },
+  ],
+  speakingLangs: [
+    {
+      name: "English",
+      fluencyPercent: "70",
+      certificate: {
+        name: "IELTS",
+        score: 6.5,
+      },
+    },
+  ],
+  carrierTitle: "Software Developer",
+  summary:
+    "Accomplished Software developer eager to leverage 6+ years of success in Information Technology",
+};
